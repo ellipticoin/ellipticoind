3949f1bf606f7b7ad87fcd4aa2a0bec02c8e5ff9
Mason Fischer <mason@kissr.co>
2020-04-26 19:51:10 -0400

    Exctract spawn_read_loop

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,3 +1,4 @@
+#![recursion_limit = "256"]
 use async_std::net::SocketAddr;
 use async_std::net::TcpListener;
 use async_std::net::TcpStream;
@@ -32,6 +33,18 @@ pub struct Server {
     pub sender: futures::channel::mpsc::Sender<Vec<u8>>,
 }
 
+pub async fn spawn_read_loop(
+    mut read_half: futures::io::ReadHalf<TcpStream>,
+    mut sender: futures::channel::mpsc::UnboundedSender<Vec<u8>>,
+) {
+    task::spawn(async move {
+        loop {
+            let mut buf = vec![0u8; 4];
+            read_half.read(&mut buf).await.unwrap();
+            sender.send(buf).await.unwrap();
+        }
+    });
+}
 impl Server {
     pub async fn listen(
         &mut self,
@@ -41,66 +54,46 @@ impl Server {
         bootnodes: Vec<SocketAddr>,
     ) {
         let listener = TcpListener::bind(address).await.unwrap();
-        let (read_sender, mut read_receiver) = async_std::sync::channel::<Vec<u8>>(1);
-        let (mut read_sender2, mut read_receiver2) = futures::channel::mpsc::unbounded();
-        let (write_sender, mut write_receiver) = async_std::sync::channel::<futures::io::WriteHalf<TcpStream>>(1);
+        let (read_sender, mut read_receiver) = futures::channel::mpsc::unbounded();
+        let (stream_sender, mut stream_receiver) = async_std::sync::channel::<TcpStream>(1);
         task::spawn(async move {
             let mut streams = vec![];
-            // let mut streams2 = vec![];
             for bootnode in bootnodes {
                 let stream = TcpStream::connect(bootnode).await.unwrap();
-                let (mut read_half, write_half) = stream.split(); 
+                let (read_half, write_half) = stream.split();
                 streams.push(write_half);
-                // let reader = async_std::io::BufReader::new(read_half);
-                // futures::StreamExt::fuse(reader.lines()).await;
-                // let mut lines_from_server = reader.lines();
-                let mut sender2 = read_sender2.clone();
-                task::spawn(async move {
-                    loop {
-                        let mut buf = vec![0u8; 4];
-                        read_half.read(&mut buf).await.unwrap();
-                        sender2.send(buf).await.unwrap();
-                    }
-                    // sender.send(buf).await;
-                });
-
-                // streams2.push(read_half);
-
+                spawn_read_loop(read_half, read_sender.clone()).await;
             }
-            let mut next_write_receiver_fused =  write_receiver.next().fuse();
-            let mut next_read_receiver_fused =  read_receiver.next().fuse();
-            let mut next_read_receiver2_fused =  read_receiver2.next().fuse();
-            loop{
-            select! {
-                stream = next_write_receiver_fused => {
-                    streams.push(stream.expect("no stream"));
-                },
-                message = next_read_receiver_fused => {
-                    sender.send(message.unwrap().clone()).await;
-                },
-                message = next_read_receiver2_fused => {
-                    sender.send(message.unwrap().clone()).await;
-                },
-                message = receiver.next() => {
-                    if let Some(message) = message {
-                        for mut stream in &mut streams {
-                            stream.write_all(&message.clone()).await.expect("failed to write");
+            let mut next_stream_receiver_fused = stream_receiver.next().fuse();
+            let mut next_read_receiver_fused = read_receiver.next().fuse();
+            loop {
+                select! {
+                    stream = next_stream_receiver_fused => {
+                        let (mut read_half, write_half) = stream.expect("1").split();
+                        spawn_read_loop(read_half,read_sender.clone()).await;
+                        streams.push(write_half);
+                    },
+                    message = next_read_receiver_fused => {
+                        if let Some(message) = message {
+                            sender.send(message.clone()).await;
                         }
-                    }
-                },
-                complete => (),
-            }}
+                    },
+                    message = receiver.next() => {
+                        if let Some(message) = message {
+                            for mut stream in &mut streams {
+                                stream.write_all(&message.clone()).await.expect("failed to write");
+                            }
+                        }
+                    },
+                    complete => (),
+                }
+            }
         });
 
         task::spawn(async move {
             let mut incoming = listener.incoming();
-            while let Some(stream) = incoming.next().await {
-                let stream = stream.unwrap();
-                let mut buf = vec![0u8; 4];
-                let (mut read_half, write_half) = stream.split();
-                read_half.read(&mut buf).await.unwrap();
-                read_sender.send(buf).await;
-                write_sender.send(write_half).await;
+            while let Some(Ok(stream)) = incoming.next().await {
+                stream_sender.send(stream).await;
             }
         });
     }
@@ -136,9 +129,9 @@ mod tests {
     use super::*;
     use libp2p::identity::ed25519;
     // use serde::de::DeserializeOwned;
+    use async_std::sync::channel;
     use serde::Deserialize;
     use serde::Serialize;
-    use async_std::sync::channel;
 
     #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
     pub enum Message {
@@ -165,7 +158,7 @@ mod tests {
         let (mut alices_sender, mut alices_receiver) = alices_server.split();
 
         let (s2, r2) = channel::<Vec<u8>>(1);
-        let (s3, r3) =  futures::channel::mpsc::channel::<Vec<u8>>(1);
+        let (s3, r3) = futures::channel::mpsc::channel::<Vec<u8>>(1);
         let mut bobs_server = Server {
             private_key: bobs_key.encode().clone().to_vec(),
             bootnodes: vec!["0.0.0.0:1234".parse().unwrap()],
@@ -182,9 +175,21 @@ mod tests {
             )
             .await;
         let (mut bobs_sender, mut bobs_receiver) = bobs_server.split();
-        bobs_sender.send("test\n".as_bytes().to_vec()).await.unwrap();
-        assert_eq!(alices_receiver.next().await.unwrap(), "test".as_bytes().to_vec());
-        alices_sender.send("boom\n".as_bytes().to_vec()).await.unwrap();
-        assert_eq!(bobs_receiver.next().await.unwrap(), "boom".as_bytes().to_vec());
+        bobs_sender
+            .send("test".as_bytes().to_vec())
+            .await
+            .unwrap();
+        assert_eq!(
+            alices_receiver.next().await.unwrap(),
+            "test".as_bytes().to_vec()
+        );
+        alices_sender
+            .send("boom".as_bytes().to_vec())
+            .await
+            .unwrap();
+        assert_eq!(
+            bobs_receiver.next().await.unwrap(),
+            "boom".as_bytes().to_vec()
+        );
     }
 }
