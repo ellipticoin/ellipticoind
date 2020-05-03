665b68d6c803df51c651bcbad6db84abc958cf07
Mason Fischer <mason@kissr.co>
2020-05-03 13:00:28 -0400

    Connect to new peers

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,11 +1,13 @@
 #![recursion_limit = "256"]
 pub extern crate serde;
+extern crate rand;
 use async_std::net::SocketAddr;
 use async_std::net::TcpListener;
 use async_std::net::TcpStream;
 use async_std::pin::Pin;
 pub use async_std::sync;
 use async_std::task;
+use rand::seq::SliceRandom;
 use futures::channel::mpsc;
 use futures::io::WriteHalf;
 use futures::prelude::*;
@@ -29,7 +31,8 @@ pub struct Sender {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Protocol {
-    NewPeer((SocketAddr, Vec<u8>)),
+    Hello(SocketAddr),
+    NewPeer(SocketAddr),
     Message(Vec<u8>),
 }
 
@@ -75,27 +78,49 @@ impl<
         let socket_addr = self.socket_addr;
         let bootnodes = self.bootnodes;
         let listener = TcpListener::bind(socket_addr).await.unwrap();
-        let (read_sender, mut read_receiver) = mpsc::unbounded();
+        let (mut read_sender, mut read_receiver) = mpsc::unbounded();
         let (stream_sender, mut stream_receiver) = mpsc::channel::<TcpStream>(1);
         let (outgoing_sender, mut outgoing_receiver) = self.outgoing_channel;
         let (mut incommming_sender, incomming_receiver) = self.incommming_channel;
         task::spawn(async move {
             let mut streams = vec![];
-            for stream in stream::iter(bootnodes).map(TcpStream::connect).next().await {
-                handle_stream(stream.await.unwrap(), &mut streams, read_sender.clone()).await;
-            }
+            let random_bootnode = bootnodes.choose(&mut rand::thread_rng());
+            if let Some(bootnode) = random_bootnode {
+                let mut stream = TcpStream::connect(bootnode).await.unwrap();
+                let outgoing_message_bytes = serde_cbor::to_vec(&Protocol::Hello(
+                        socket_addr
+                        ))
+                    .unwrap();
+                let length_bytes = u32::to_le_bytes(outgoing_message_bytes.len() as u32);
+                use std::time::Duration;
+
+use async_std::task;
+
+                stream.write_all(&length_bytes).await.unwrap();
+                stream.write_all(&outgoing_message_bytes).await.unwrap();
+                task::sleep(Duration::from_secs(1)).await;
+                handle_stream(stream, &mut streams, read_sender.clone()).await;
+                // let (read_half, write_half) = stream.split();
+                // streams.push(write_half);
+                // spawn_read_loop(read_half, read_sender.clone()).await;
+
+            };
+            
             loop {
                 let mut next_stream_receiver_fused = stream_receiver.next().fuse();
                 let mut next_read_receiver_fused = read_receiver.next().fuse();
                 select! {
-                    stream = next_stream_receiver_fused =>
-                        handle_stream(stream.unwrap(), &mut streams, read_sender.clone()).await
-                    ,
-                    incommming_message = next_read_receiver_fused =>
-                        handle_incomming_message(incommming_message.unwrap(), &mut incommming_sender).await,
+                    stream = next_stream_receiver_fused =>{
+                        if let Some(stream) = stream {
+                            // broadcast_new_peer(&mut streams, stream.local_addr().unwrap()).await;
+                            handle_stream(stream, &mut streams, read_sender.clone()).await;
+                        }
+                },
+                    incommming_message = next_read_receiver_fused =>{
+                        handle_incomming_message(socket_addr, &mut streams, incommming_message.unwrap(), &mut incommming_sender, &mut read_sender).await;},
                     outgoing_message = outgoing_receiver.next() =>{
                         if let Some(outgoing_message) = outgoing_message {
-                        handle_outgoing_message(outgoing_message, &mut streams).await
+                        handle_outgoing_message(&mut streams, outgoing_message).await
                         }},
                     complete => (),
                 }
@@ -115,14 +140,36 @@ impl<
     }
 }
 
+async fn broadcast_new_peer(
+    socket_addr: SocketAddr,
+    streams: &mut Vec<(SocketAddr, WriteHalf<TcpStream>)>,
+    peer: SocketAddr,
+) {
+    for (stream_addr, stream) in streams {
+        if (socket_addr.eq(stream_addr)) {
+            break;
+        }
+        let outgoing_message_bytes = serde_cbor::to_vec(&Protocol::NewPeer(
+                peer
+        ))
+        .unwrap();
+        let length_bytes = u32::to_le_bytes(outgoing_message_bytes.len() as u32);
+        stream.write_all(&length_bytes).await.unwrap();
+        stream.write_all(&outgoing_message_bytes).await.unwrap();
+    }
+}
+
 async fn handle_outgoing_message<
     S: Clone + Serialize + std::marker::Send + 'static + std::marker::Sync,
 >(
+    streams: &mut Vec<(SocketAddr, WriteHalf<TcpStream>)>,
     outgoing_message: S,
-    streams: &mut Vec<WriteHalf<TcpStream>>,
 ) {
-    for stream in streams {
-        let outgoing_message_bytes = serde_cbor::to_vec(&outgoing_message).unwrap();
+    for (_, stream) in streams {
+        let outgoing_message_bytes = serde_cbor::to_vec(&Protocol::Message(
+            serde_cbor::to_vec(&outgoing_message).unwrap(),
+        ))
+        .unwrap();
         let length_bytes = u32::to_le_bytes(outgoing_message_bytes.len() as u32);
         stream.write_all(&length_bytes).await.unwrap();
         stream.write_all(&outgoing_message_bytes).await.unwrap();
@@ -130,22 +177,38 @@ async fn handle_outgoing_message<
 }
 
 async fn handle_incomming_message<D: DeserializeOwned + std::marker::Send + 'static>(
+    socket_addr: SocketAddr,
+    mut streams: &mut Vec<(SocketAddr, WriteHalf<TcpStream>)>,
     incommming_message: Vec<u8>,
     incomming_sender: &mut mpsc::Sender<D>,
+    read_sender: &mut mpsc::UnboundedSender<Vec<u8>>,
 ) {
-    incomming_sender
-        .send(serde_cbor::from_slice(&incommming_message).unwrap())
-        .await
-        .unwrap();
+    match serde_cbor::from_slice(&incommming_message) {
+        Ok(Protocol::Message(incommming_message)) => {
+            incomming_sender
+                .send(serde_cbor::from_slice(&incommming_message).unwrap())
+                .await
+                .unwrap();
+        },
+        Ok(Protocol::Hello(address)) => {
+            broadcast_new_peer(socket_addr, streams, address).await;
+        },
+        Ok(Protocol::NewPeer(address)) => {
+            let stream = TcpStream::connect(address).await.unwrap();
+            handle_stream(stream, &mut streams, read_sender.clone()).await;
+        },
+        _ => (),
+    }
 }
 
 async fn handle_stream(
     stream: TcpStream,
-    streams: &mut Vec<WriteHalf<TcpStream>>,
+    streams: &mut Vec<(SocketAddr, WriteHalf<TcpStream>)>,
     read_sender: mpsc::UnboundedSender<Vec<u8>>,
 ) {
+    let addr = stream.local_addr().unwrap();
     let (read_half, write_half) = stream.split();
-    streams.push(write_half);
+    streams.push((addr, write_half));
     spawn_read_loop(read_half, read_sender.clone()).await;
 }
 
@@ -201,20 +264,29 @@ mod tests {
         let bobs_key = ed25519::Keypair::generate();
         let alices_server: Server<Message, Message> = Server::new(
             alices_key.encode().clone().to_vec(),
-            "0.0.0.0:1234".parse().unwrap(),
+            "127.0.0.1:1234".parse().unwrap(),
             vec![],
         );
         let (mut alices_sender, mut alices_receiver) = alices_server.channel().await;
 
         let bobs_server: Server<Message, Message> = Server::new(
             bobs_key.encode().clone().to_vec(),
-            "0.0.0.0:1235".parse().unwrap(),
-            vec!["0.0.0.0:1234".parse().unwrap()],
+            "127.0.0.1:1235".parse().unwrap(),
+            vec!["127.0.0.1:1234".parse().unwrap()],
         );
         let (mut bobs_sender, mut bobs_receiver) = bobs_server.channel().await;
         bobs_sender.send(message.clone()).await.unwrap();
         assert_eq!(alices_receiver.next().await.unwrap(), expected_message);
         alices_sender.send(message.clone()).await.unwrap();
         assert_eq!(bobs_receiver.next().await.unwrap(), expected_message);
+        let carols_key = ed25519::Keypair::generate();
+        let carols_server: Server<Message, Message> = Server::new(
+            carols_key.encode().clone().to_vec(),
+            "127.0.0.1:1236".parse().unwrap(),
+            vec!["127.0.0.1:1235".parse().unwrap()],
+        );
+        let (mut carols_sender, _carols_receiver) = carols_server.channel().await;
+        carols_sender.send(message.clone()).await.unwrap();
+        assert_eq!(alices_receiver.next().await.unwrap(), expected_message);
     }
 }
