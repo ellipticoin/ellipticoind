143bc51cd5adf71115a2e654084e2858f0569121
Mason Fischer <mason@kissr.co>
2020-06-16 18:08:11 -0400

    Add more verbose message about broadcast failures

--- src/api/memory.rs
+++ src/api/memory.rs
@@ -6,8 +6,8 @@ use tide::Response;
 pub async fn show(req: tide::Request<ApiState>) -> Response {
     let key: String = req.param("key").unwrap_or("".to_string());
     let mut redis = req.state().redis.get().unwrap();
-    if let Ok(value) = redis
-        .get::<Vec<u8>, Vec<u8>>(base64::decode_config(&key, base64::URL_SAFE).unwrap())
+    if let Ok(value) =
+        redis.get::<Vec<u8>, Vec<u8>>(base64::decode_config(&key, base64::URL_SAFE).unwrap())
     {
         Response::new(200).body(Body::from(serde_cbor::to_vec(&value).unwrap()))
     } else {
