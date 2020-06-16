d11ff949fc9d74dbd6d9c85eccfd4d2d89510c17
Mason Fischer <mason@kissr.co>
2020-06-16 18:08:11 -0400

    Add more verbose message about broadcast failures

--- src/api/storage.rs
+++ src/api/storage.rs
@@ -5,8 +5,7 @@ use tide::Response;
 pub async fn show(req: tide::Request<ApiState>) -> Response {
     let key: String = req.param("key").unwrap_or("".to_string());
     let rocksdb = &req.state().rocksdb;
-    if let Ok(value) = rocksdb
-        .get(base64::decode_config(&key, base64::URL_SAFE).unwrap_or(vec![]))
+    if let Ok(value) = rocksdb.get(base64::decode_config(&key, base64::URL_SAFE).unwrap_or(vec![]))
     {
         Response::new(200).body(Body::from(serde_cbor::to_vec(&value).unwrap_or(vec![])))
     } else {
--- src/src/env.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-#[derive(Deserialize, Serialize, Clone, Debug, Default)]
-pub struct Env {
-    pub block_number: u64,
-    #[serde(with = "serde_bytes")]
-    pub block_winner: Vec<u8>,
-    pub caller: Option<serde_bytes::ByteBuf>,
-}
