1d330bb86e349227f26cabf01f7fc6419562d2dc
Mason Fischer <mason@kissr.co>
2020-07-01 12:53:49 -0400

    Proxy requests to the current miner

--- src/api/helpers.rs
+++ src/api/helpers.rs
@@ -10,6 +10,36 @@ pub fn base64_param(req: &Request<State>, key: &str) -> Result<Vec<u8>> {
         .map_err(|err| Error::new(StatusCode::BadRequest, err))
 }
 
+pub async fn proxy_get(req: &Request<State>, proxy_url: String) -> Result<Response> {
+    let mut url = req.url().clone();
+    let host = proxy_url.split(":").next().unwrap();
+    let port = proxy_url.split(":").last().unwrap().parse().unwrap();
+    url.set_host(Some(&host)).unwrap();
+    url.set_port(Some(port)).unwrap();
+    let mut surf_res = surf::get(url).await.unwrap();
+    let mut res = Response::new(surf_res.status());
+    let body = surf_res.body_bytes().await.unwrap();
+    res.set_body(Body::from_bytes(body));
+    Ok(res)
+}
+
+pub async fn proxy_post(
+    req: &Request<State>,
+    proxy_url: String,
+    body: Vec<u8>,
+) -> Result<Response> {
+    let mut url = req.url().clone();
+    let host = proxy_url.split(":").next().unwrap();
+    let port = proxy_url.split(":").last().unwrap().parse().unwrap();
+    url.set_host(Some(&host)).unwrap();
+    url.set_port(Some(port)).unwrap();
+    let mut surf_res = surf::post(url).body_bytes(&body).await.unwrap();
+    let mut res = Response::new(surf_res.status());
+    let body = surf_res.body_bytes().await.unwrap();
+    res.set_body(Body::from_bytes(body));
+    Ok(res)
+}
+
 pub async fn body<T: serde::de::DeserializeOwned>(
     req: &mut tide::Request<State>,
 ) -> serde_cbor::Result<T> {
