30c122914771324051445c14ca192428a5652b04
Mason Fischer <mason@kissr.co>
2020-08-12 09:01:25 -0400

    Move sse endpoint into the blocks module

--- src/api/addresses.rs
+++ src/api/addresses.rs
@@ -1,5 +1,9 @@
 use super::State;
-use crate::{api::helpers::to_cbor_response, config::get_pg_connection, models::highest_nonce};
+use crate::{
+    api::helpers::{base64_decode, to_cbor_response},
+    config::get_pg_connection,
+    models::highest_nonce,
+};
 use serde::Serialize;
 use tide::{Response, Result};
 
@@ -11,10 +15,7 @@ pub struct Address {
 pub async fn show(req: tide::Request<State>) -> Result<Response> {
     let con = get_pg_connection();
     let address: String = req.param("address").unwrap();
-    let highest_nonce: Option<i64> = highest_nonce(
-        &con,
-        base64::decode_config(&address, base64::URL_SAFE).unwrap(),
-    );
+    let highest_nonce: Option<i64> = highest_nonce(&con, base64_decode(&address).unwrap());
     Ok(to_cbor_response(&Address {
         highest_nonce: highest_nonce
             .map(|highest_nonce| serde_cbor::Value::Integer(highest_nonce as i128)),
