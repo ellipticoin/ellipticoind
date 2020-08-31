39f9ac62a6f54a12566edfe955dc4a84d669d5ac
Mason Fischer <mason@kissr.co>
2020-08-31 16:36:48 -0400

    Add remove_liqidity endpoint

--- src/system_contracts/exchange/errors.rs
+++ src/system_contracts/exchange/errors.rs
@@ -5,4 +5,8 @@ lazy_static! {
         code: 1,
         message: "Pool not found. Please create this pool and try again".to_string(),
     };
+    pub static ref INSUFFICIENT_BALANCE: Error = Error {
+        code: 2,
+        message: "Insufficient balance".to_string(),
+    };
 }
