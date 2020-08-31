d3a88330297504c90392be8a5a47c9f1b0c4f0c1
Mason Fischer <mason@kissr.co>
2020-08-31 12:19:32 -0400

    Fix bug in hash onion generation

--- src/models/hash_onion.rs
+++ src/models/hash_onion.rs
@@ -1,22 +1,19 @@
 use crate::{
     diesel::{QueryDsl, RunQueryDsl},
     helpers::sha256,
-    schema::hash_onion::dsl::*,
+    schema::{hash_onion, hash_onion::dsl::*},
 };
 use diesel::{
-    dsl::*,
+    dsl::{sql_query, *},
+    prelude::*,
     r2d2::{ConnectionManager, PooledConnection},
     PgConnection,
 };
 pub use diesel_migrations::revert_latest_migration;
 use indicatif::ProgressBar;
 use rand::Rng;
-
-use crate::schema::hash_onion;
-use std::env;
-
-use diesel::{dsl::sql_query, prelude::*};
 use serde::{Deserialize, Serialize};
+use std::env;
 
 #[derive(
     Queryable,
@@ -41,7 +38,7 @@ impl HashOnion {
             .select(layer)
             .order(id.desc())
             .first::<Vec<u8>>(pg_db)
-            .unwrap();
+            .expect("No layers left on hash onion");
         sql_query(
             "delete from hash_onion where id in (
         select id from hash_onion order by id desc limit 1
@@ -57,12 +54,10 @@ impl HashOnion {
             .map(|hash_onion_size| hash_onion_size.parse().unwrap())
             .unwrap_or(31 * 24 * 60 * 60);
         let sql_query_size = 65534;
-        let center: Vec<u8> = rand::thread_rng()
+        let mut center: Vec<u8> = rand::thread_rng()
             .sample_iter(&rand::distributions::Standard)
             .take(32)
             .collect();
-        let mut onion = vec![center];
-
         println!("Generating Hash Onion");
         let pb = ProgressBar::new(hash_onion_size);
         pb.set_style(
@@ -72,8 +67,10 @@ impl HashOnion {
         );
         for _ in (0..hash_onion_size).step_by(sql_query_size) {
             pb.inc(sql_query_size as u64);
-            for _ in 1..(sql_query_size) {
-                onion.push(sha256(onion.last().unwrap().to_vec()).to_vec());
+            let mut onion: Vec<Vec<u8>> = vec![];
+            for _ in 0..(sql_query_size) {
+                center = sha256(center.to_vec().clone()).to_vec();
+                onion.push(center.clone());
             }
             let values: Vec<HashOnion> = onion
                 .iter()
@@ -83,7 +80,6 @@ impl HashOnion {
                 .collect();
             let query = insert_into(hash_onion).values(&values);
             query.execute(db).unwrap();
-            onion = vec![onion.last().unwrap().to_vec()];
         }
         pb.finish();
     }
