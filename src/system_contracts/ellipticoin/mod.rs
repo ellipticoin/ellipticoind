cffff25c1def25e2174bc2406b263ac8b0672256
Mason Fischer <mason@kissr.co>
2020-09-01 16:50:11 -0400

    Use storage_accessors macro is ellipticoin contract

--- src/system_contracts/ellipticoin/mod.rs
+++ src/system_contracts/ellipticoin/mod.rs
@@ -3,7 +3,7 @@ mod ethereum;
 mod hashing;
 
 use crate::helpers::zero_pad_vec;
-use ellipticoin::{constants::SYSTEM_ADDRESS, Address, Token};
+use ellipticoin::{constants::SYSTEM_ADDRESS, storage_accessors, Address, Token};
 use errors::Error;
 use hashing::sha256;
 use rand::{rngs::SmallRng, seq::SliceRandom, SeedableRng};
@@ -12,18 +12,13 @@ use serde_cbor::{value::to_value, Value};
 use std::convert::TryInto;
 use wasm_rpc_macros::export_native;
 
-enum Namespace {
-    _Allowances,
-    #[cfg(test)]
-    _Balances,
-    #[cfg(not(test))]
-    _Balances,
-    BlockNumber,
-    EthereumBalances,
-    Miners,
-    UnlockedEthereumBalances,
-    TotalUnlockedEthereum,
-}
+storage_accessors!(
+    block_number: u64,
+    ethereum_balances: u64,
+    miners: Vec<Miner>,
+    unlocked_ethereum_balances: bool,
+    total_unlocked_ethereum: u64,
+);
 
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Miner {
@@ -36,7 +31,7 @@ pub struct Miner {
 export_native! {
     pub fn transfer_to_current_miner<API: ellipticoin::API>(api: &mut API, amount: u64) -> Result<u64, Box<Error>> {
         let token_id: [u8; 32] = zero_pad_vec("ELC".as_bytes(), 32)[..].try_into().unwrap();
-        let miners = get_miners(api);
+        let miners = get_miners(api, vec![]);
         let current_miner = miners.first().unwrap().address.clone();
         api.call(
             SYSTEM_ADDRESS,
@@ -65,29 +60,19 @@ export_native! {
             encoded_ellipticoin_adress
         );
         let address = ethereum::ecrecover_address(message.as_bytes(), &unlock_signature);
-        if api.get_storage([[Namespace::UnlockedEthereumBalances as u8].to_vec(), address.clone()].concat()).unwrap_or(false) {
+        if get_unlocked_ethereum_balances(api, address.clone()) {
             return Err(Box::new(errors::BALANCE_ALREADY_UNLOCKED.clone()));
         };
-        let balance: u64 =
-            api.get_storage::<_, u64>([[Namespace::EthereumBalances as u8].to_vec(), address.clone()].concat()).unwrap_or(0) * 100;
-
-        let mut total_unlocked_ethereum: u64 =
-            api.get_storage::<_, u64>([Namespace::TotalUnlockedEthereum as u8].to_vec()).unwrap_or(0);
+        let balance: u64 = get_ethereum_balances(api, address.clone());
+        let mut total_unlocked_ethereum: u64 = get_total_unlocked_ethereum(api, vec![]);
         if total_unlocked_ethereum + balance > 1000000 * 10000 {
             return Err(Box::new(errors::BALANCE_EXCEEDS_THIS_PHASE.clone()));
         } else {
             total_unlocked_ethereum += balance;
-            api.set_storage::<_, u64>(
-                [Namespace::TotalUnlockedEthereum as u8].to_vec(),
-                total_unlocked_ethereum,
-            );
+            set_total_unlocked_ethereum(api, vec![], total_unlocked_ethereum);
         }
         credit(api, Address::PublicKey(ellipticoin_address), balance);
-        api.set_storage([
-            [Namespace::UnlockedEthereumBalances as u8].to_vec(),
-            address].concat(),
-            true
-        );
+        set_unlocked_ethereum_balances(api, address, true);
 
         Ok(balance.into())
     }
@@ -98,19 +83,19 @@ export_native! {
         burn_per_block: u64,
         hash_onion_skin: [u8; 32],
     ) -> Result<Value, Box<Error>> {
-        let mut miners = get_miners(api);
+        let mut miners = get_miners(api, vec![]);
         miners.push(Miner {
             address: api.caller().as_public_key().unwrap(),
             host,
             burn_per_block,
             hash_onion_skin,
         });
-        set_miners(api, &miners);
+        set_miners(api, vec![], miners);
         Ok(Value::Null)
     }
 
     pub fn reveal<API: ellipticoin::API>(api: &mut API, value: [u8; 32]) -> Result<Vec<Miner>, Box<Error>> {
-        let mut miners = get_miners(api, );
+        let mut miners = get_miners(api, vec![]);
         if api.caller() != ellipticoin::Address::PublicKey(miners.first().unwrap().address) {
             return Err(Box::new(errors::SENDER_IS_NOT_THE_WINNER.clone()));
         }
@@ -132,8 +117,8 @@ export_native! {
     }
 
     fn increment_block_number<API: ellipticoin::API>(api: &mut API) {
-        let block_number = api.get_storage::<_, u32>([Namespace::BlockNumber as u8].to_vec()).unwrap_or(0);
-        api.set_storage::<_, u32>([Namespace::BlockNumber as u8].to_vec(), block_number + 1);
+        let block_number = get_block_number(api, vec![]);
+        set_block_number(api, vec![], block_number + 1);
     }
 
     fn shuffle_miners<API: ellipticoin::API>(api: &mut API, miners: &mut Vec<Miner>, value: [u8; 32]) {
@@ -147,20 +132,11 @@ export_native! {
             shuffled_miners.push(random_miner.clone());
             miners.retain(|miner| miner.clone() != random_miner);
         }
-        set_miners(api, &shuffled_miners);
-        *miners = shuffled_miners;
-    }
-
-    pub fn get_miners<API: ellipticoin::API>(api: &mut API) -> Vec<Miner> {
-        api.get_storage::<_, Vec<Miner>>([Namespace::Miners as u8].to_vec()).unwrap_or(vec![])
-    }
-
-    fn set_miners<API: ellipticoin::API>(api: &mut API, miners: &Vec<Miner>) {
-        api.set_storage::<_, Value>([Namespace::Miners as u8].to_vec(), to_value(miners).unwrap());
+        set_miners(api, vec![], shuffled_miners)
     }
 
     fn settle_block_rewards<API: ellipticoin::API>(api: &mut API) {
-        let miners = get_miners(api);
+        let miners = get_miners(api, vec![]);
         let winner = miners.first().as_ref().unwrap().clone();
         for miner in &miners {
             credit(api, ellipticoin::Address::PublicKey(winner.address.clone()), miner.burn_per_block);
@@ -223,7 +199,6 @@ mod tests {
         helpers::generate_hash_onion,
         system_contracts::test_api::{TestAPI, TestState},
     };
-    use ellipticoin::API;
     use std::env;
 
     use ellipticoin_test_framework::constants::actors::{ALICE, ALICES_PRIVATE_KEY, BOB};
@@ -265,10 +240,6 @@ mod tests {
         assert!(reveal(&mut api, *bobs_onion.last().unwrap()).is_ok());
         assert_eq!(api.get_balance(token.clone(), *ALICE), 6);
         assert_eq!(api.get_balance(token, *BOB), 4);
-        assert_eq!(
-            api.get_storage::<_, u64>([Namespace::BlockNumber as u8].to_vec())
-                .unwrap(),
-            3
-        );
+        assert_eq!(get_block_number(&mut api, vec![]), 3);
     }
 }
