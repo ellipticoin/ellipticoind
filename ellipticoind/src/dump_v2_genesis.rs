5c31ee30c27497bf711969d7ea88567462bc162d
Mason Fischer <mason@kissr.co>
2021-03-30 11:05:52 -0400

    wip

--- ellipticoind/src/dump_v2_genesis.rs
+++ ellipticoind/src/dump_v2_genesis.rs
@@ -261,8 +261,10 @@ pub async fn run_transactions_in_db() {
         .load::<Transaction>(&pg_db)
         .unwrap();
     let mut state = IN_MEMORY_STATE.lock().await;
-    let mut expected_total_supply = 0;
-    let magic_number = serde_cbor::value::to_value(51495).unwrap();
+    // let mut expected_total_supply = 0;
+    let mut total_minted = 0;
+    let mut total_released = 0;
+    // let magic_number = serde_cbor::value::to_value(51495).unwrap();
     for mut transaction in transactions {
         if transaction.id >= 3104941 {
             break;
@@ -276,27 +278,42 @@ id: V1_ETH.to_vec().into(),
 },
 );
 
-            legacy::run(&mut api, &mut transaction).await;
-            let arguments: Vec<serde_cbor::Value> = serde_cbor::from_slice(&transaction.arguments).unwrap();
-           if arguments.len() == 3 {
-            if arguments[2] == magic_number {
-            println!("{}", transaction.id);
-            }
-           }
-//         let total_supply_after = crate::system_contracts::token::get_total_supply(
-//                 &mut api,
-//                 ellipticoin::Token {
-// issuer: "Bridge".into(),
-// id: V1_ETH.to_vec().into(),
-// },
-// );
+           let return_value =  legacy::run(&mut api, &mut transaction).await;
+           //  let arguments: Vec<serde_cbor::Value> = serde_cbor::from_slice(&transaction.arguments).unwrap();
+           // if arguments.len() == 3 {
+           //  if arguments[2] == magic_number {
+           //  println!("{}", transaction.id);
+           //  }
+           // }
 //         if transaction.function == "release" {
             // println!("{} {} {} {:?}", transaction.function, total_supply_before, total_supply_after, return_value);
         // }
         
-        // if ["mint", "release"].contains(&transaction.function.as_ref()) {
-        //     let token = serde_cbor::value::from_value::<serde_bytes::ByteBuf>(serde_cbor::from_slice::<Vec<serde_cbor::Value>>(&transaction.arguments).unwrap()[0].clone()).unwrap();
-        // let return_value2: Result<serde_cbor::Value, serde_cbor::Value> = serde_cbor::value::from_value(return_value.clone()).unwrap();
+        if ["mint", "release"].contains(&transaction.function.as_ref()) {
+            let token = serde_cbor::value::from_value::<serde_bytes::ByteBuf>(serde_cbor::from_slice::<Vec<serde_cbor::Value>>(&transaction.arguments).unwrap()[0].clone()).unwrap();
+            if token.to_vec() == V1_ETH {
+                let amount = serde_cbor::value::from_value::<u64>(serde_cbor::from_slice::<Vec<serde_cbor::Value>>(&transaction.arguments).unwrap()[2].clone()).map(|amount| amount.to_string()).unwrap_or("NaN".to_string());
+                let recipient = serde_cbor::value::from_value::<serde_bytes::ByteBuf>(serde_cbor::from_slice::<Vec<serde_cbor::Value>>(&transaction.arguments).unwrap()[1].clone()).unwrap();
+                let return_value2: Result<serde_cbor::Value, serde_cbor::Value> = serde_cbor::value::from_value(return_value.clone()).unwrap();
+                if return_value2.is_err() {
+                    println!("ERROR {} {} {}", transaction.function, amount, hex::encode(recipient.to_vec()));
+                } else {
+                    if  transaction.function == "mint" {
+                        total_minted += amount.parse().unwrap_or(0);
+                    } else {
+                        total_minted -= amount.parse().unwrap_or(0);
+                    }
+        let total_supply_after = crate::system_contracts::token::get_total_supply(
+                &mut api,
+                ellipticoin::Token {
+issuer: "Bridge".into(),
+id: V1_ETH.to_vec().into(),
+},
+);
+                    println!("{} {} {} {} {}", transaction.function, amount, total_minted, total_supply_after, hex::encode(recipient.to_vec()));
+                }
+            }
+        }
         //     // println!("is err {}", return_value2.is_err());
         //     if token.to_vec() == V1_ETH && !return_value2.is_err() {
         //         let amount = if transaction.function == "mint" {
@@ -345,11 +362,11 @@ id: V1_ETH.to_vec().into(),
         //     println!("id: {} ETH Price {} BTC Price {}", transaction.id, eth_price as f64 / BASE_FACTOR as f64, btc_price as f64 / BASE_FACTOR as f64);
         // }
         if transaction.id % 10000 == 0 && transaction.id != 0 {
-            println!(
-                "Applied transactions #{}-#{}",
-                transaction.id - 10000,
-                transaction.id
-            )
+            // println!(
+            //     "Applied transactions #{}-#{}",
+            //     transaction.id - 10000,
+            //     transaction.id
+            // )
         };
     }
 }
